52cd5d09057e575f13f850e8d231b46dd663aa3a
Administrator <administrator@Pascals-MacBook-Pro.local>
2014-01-02 16:42:14 +0100

    more stuff... no CLI yet.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var bot = require("./lib/twitterbot");
+var bot = require("./lib/twitterbot2");
 
 
 TB = new bot({
 	consumer: {
-		key: "lxUhNzsE6d85YsBv0NNHw",
-		secret: "liIkbDQkYHvagM8Q2KWbjXwXfvEoBa2cG3WblfTziV0"
+		key: "p4cqHjb9LSH93FpwAuqsbQ",
+		secret: "4tyxmtpHvhklfo2lS1tWjBkluzt9iKZs1vkDQnngU"
 	},
 	access: {
-		token: "1891886281-nS9sHfUQkuf2eDiInIEs31CdFUL2hDB4jQDHHIa",
-		secret: "i1dOb7dZ8g5nsb0J1pIgKkXmJHYt1n1SFSIAIh3hU"
+		token: "158013661-YD3FlmOc29TQthpZanEpXctAmThfwfarZZyeB4qA",
+		secret: "Tn7A4pjp0Lxk9kEvuvAchcqzWlokhAPJrEmzV6GqPp4BX"
 	}
 });
 TB.start()
